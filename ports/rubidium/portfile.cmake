322f75a514f60df642ba842171784cfb9a201ed5
Michael Galuza <riddlermichael@yandex.ru>
2023-12-30 09:18:35 +0700

    add vcpkg features

--- ports/rubidium/portfile.cmake
+++ ports/rubidium/portfile.cmake
@@ -2,33 +2,42 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO riddlermichael/rubidium
     REF master
-    SHA512 5ded16099ba7c776f298966cd13aa64c4765f2342bf43ed26a0dd3a5e9c58c35f04c81dbaa02e0ed0ea235270e0bdf64285e051c0e825cb558daa5fbead99dd7
-)
+    SHA512 5ded16099ba7c776f298966cd13aa64c4765f2342bf43ed26a0dd3a5e9c58c35f04c81dbaa02e0ed0ea235270e0bdf64285e051c0e825cb558daa5fbead99dd7)
+
+vcpkg_check_features(
+    OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        duration-has-nan DURATION_HAS_NAN
+        full-source-location-info FULL_SOURCE_LOCATION_INFO
+        time-v2 TIME_V2)
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-    -DBUILD_TESTING=OFF
-    -DBUILD_DOC=OFF
-    -DBUILD_EXAMPLES=OFF
-)
+        ${FEATURE_OPTIONS}
+        -DBUILD_TESTING=OFF
+        -DBUILD_DOC=OFF
+        -DBUILD_EXAMPLES=OFF)
 
 vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup(
     CONFIG_PATH lib/cmake/RbC
-    DO_NOT_DELETE_PARENT_CONFIG_PATH
-)
+    DO_NOT_DELETE_PARENT_CONFIG_PATH)
 vcpkg_cmake_config_fixup(
     CONFIG_PATH lib/cmake/Rb
-    DO_NOT_DELETE_PARENT_CONFIG_PATH
-)
+    DO_NOT_DELETE_PARENT_CONFIG_PATH)
 
 file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/debug/include"
     "${CURRENT_PACKAGES_DIR}/debug/lib/cmake"
-    "${CURRENT_PACKAGES_DIR}/lib/cmake"
-)
+    "${CURRENT_PACKAGES_DIR}/lib/cmake")
 
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
-configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
+file(INSTALL "${SOURCE_PATH}/LICENSE"
+    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+    RENAME copyright)
+
+configure_file(
+    "${CMAKE_CURRENT_LIST_DIR}/usage"
+    "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage"
+    COPYONLY)
